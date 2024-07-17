5fd651ddfb5bce4d8d5d629c46637f3007cb82f1
Md Rizwan <rizwandev99@gmail.com>
2024-07-17 22:30:27 +0530

    added routing and db schema's

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "hkt";
+
+module.exports = JWT_SECRET;
---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://admin:1234@cluster0.uzbzi7b.mongodb.net/");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+};
